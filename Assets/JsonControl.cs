d7bed4647330cd3feecfb4878aaa35fc651d0729
jonas b <jonas.aksel@gmail.com>
2018-10-15 14:51:46 +0200

    2nd comit

--- Assets/JsonControl.cs
+++ Assets/JsonControl.cs
@@ -3,31 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class JsonControl : MonoBehaviour {
+public  class JsonControl : MonoBehaviour {
 
 
     public JsonObject jsonFile;
     public GameObject prefab;
     public GameObject prefab2;
+    public int count;
+    public static int numberOfFrames =343;
  
-    public static SoFrame[] frame = new SoFrame[400];
+    public static SoFrame[] frame = new SoFrame[numberOfFrames];
 
 
     // Use this for initialization
     void Start () {
+        Application.targetFrameRate = 15;
+
+
+        SetFrames();
+        NormalizeVel();
+        for (int i = 0; i < numberOfFrames; i++)
+        {
+            frame[i].CalculateSim(frame, i);
+
+         //   Debug.Log(frame[i].ClosestFrame(frame));
+        }
+       
+
+
+    }
+
+
+    private void Update()
+    {
+
    
+
+     Instantiate(prefab, new Vector3(frame[count].features[1].x, frame[count].features[1].y), Quaternion.identity);
+
+
+        Debug.Log(frame[count].ClosestFrame(frame));
+
+        count = frame[count].ClosestFrame(frame);
+    }
+
+
+
+    void SetFrames(){
+
+
         jsonFile = new JsonObject();
         string content;
 
 
 
-
-        for (int i = 0; i < 343; i++)
+        for (int i = 0; i < numberOfFrames; i++)
         {
-                content = System.IO.File.ReadAllText("Assets/output2/testVideo"+ i.ToString("0000") + "_keypoints.json");
-                JsonUtility.FromJsonOverwrite(content,jsonFile );
+            content = System.IO.File.ReadAllText("Assets/output2/testVideo" + i.ToString("0000") + "_keypoints.json");
+            JsonUtility.FromJsonOverwrite(content, jsonFile);
 
-            if(jsonFile.people.Length==1)
+            if (jsonFile.people.Length == 1)
             {
 
                 frame[i] = ScriptableObject.CreateInstance<SoFrame>();
@@ -51,30 +86,37 @@ public class JsonControl : MonoBehaviour {
                 frame[i].features[16] = new Vector2(jsonFile.people[0].pose_keypoints_2d[48], jsonFile.people[0].pose_keypoints_2d[49]); // left ear
                 frame[i].features[17] = new Vector2(jsonFile.people[0].pose_keypoints_2d[51], jsonFile.people[0].pose_keypoints_2d[52]); //right ear
 
-             
-                
+            } else{
+                frame[i] = ScriptableObject.CreateInstance<SoFrame>();
+                frame[i].isUsed = false;
+
             }
 
-            
-        }
-
-
-        for (int i = 2; i < 343; i++)
-        {
-
-            if(frame[i] !=null && frame[i-1] != null)
-            Debug.Log(i+"    "+frame[i].CalculateVelo(frame[i - 1].features[1]));
-
-
 
         }
+    }
 
-        for (int i = 0; i < 18; i++)
+
+    public void NormalizeVel(){
+        float maxVel = 0;
+   
+        for (int i = 1; i < frame.Length; i++)
         {
-            Instantiate(prefab, new Vector3(frame[3].features[i].x, frame[3].features[i].y), Quaternion.identity);
+            if(frame[i].isUsed && maxVel<frame[i].CalculateVelo(frame[i - 1]))
+            maxVel = frame[i].CalculateVelo(frame[i - 1]);
+          
+        }
+        Debug.Log(maxVel);
+
+        for (int i = 0; i < frame.Length; i++)
+        {
+            if (frame[i].isUsed)
+            {
+                frame[i].normVelo = frame[i].velo/maxVel ;
+               
+            }
         }
 
     }
-
 
 }
--- Assets/SoFrame.cs
+++ Assets/SoFrame.cs
@@ -2,29 +2,79 @@
 using System.Collections;
 
 [CreateAssetMenu(fileName = "Data", menuName = "Inventory/frame", order = 1)]
+[System.Serializable]
 public class SoFrame : ScriptableObject
 {
 
     public Vector2[] features = new Vector2[18];
     public float velo;
+    public bool isUsed = true;
+    public float normVelo;
+    public Vector2[] simi = new Vector2[343];
 
 
 
 
-    public float CalculateVelo(Vector2 index)
+    public float CalculateVelo(SoFrame prevFrame)
+    {
+        if (prevFrame.isUsed)
+            return velo = ((features[1].x - prevFrame.features[1].x));
+        else return 0;
+    }
+
+
+    public void CalculateSim(SoFrame[] frameArray, int index)
     {
 
 
-        //velo = Vector2.SqrMagnitude(features[1]-index);
+        for (int i = 0; i < frameArray.Length; i++)
+            {
 
+            if (frameArray[i].isUsed && index !=i)
+            {
 
-        if (features[1].x - index.x <= 0)
-            velo = -((features[1] - index).magnitude);
-        else velo = (features[1] - index).magnitude;
-
-
-        return velo;
+                simi[i] += features[0] - frameArray[i].features[0];
+                simi[i] += features[1] - frameArray[i].features[1];
+                simi[i] += features[2] - frameArray[i].features[2];
+                simi[i] += features[3] - frameArray[i].features[3];
+                simi[i] += features[4] - frameArray[i].features[4];
+                simi[i] += features[5] - frameArray[i].features[5];
+                simi[i] += features[6] - frameArray[i].features[6];
+                simi[i] += features[7] - frameArray[i].features[7];
+                simi[i] += features[8] - frameArray[i].features[8];
+                simi[i] += features[9] - frameArray[i].features[9];
+                simi[i] += features[10] - frameArray[i].features[10];
+                simi[i] += features[11] - frameArray[i].features[11];
+                simi[i] += features[12] - frameArray[i].features[12];
+                simi[i] += features[13] - frameArray[i].features[13];
+                simi[i] += features[14] - frameArray[i].features[14];
+                simi[i] += features[15] - frameArray[i].features[15];
+                simi[i] += features[16] - frameArray[i].features[16];
+                simi[i] += features[17] - frameArray[i].features[17];
+            } else
+            {
+                simi[i] = new Vector2(100, 100);
+            }
+        }
+       
     }
 
+    public int ClosestFrame(SoFrame[] frameArray){
+        float best=200;
+        int bestFrame =700; 
+
+        for (int i = 0; i < simi.Length; i++)
+        {
+            if (best > simi[i].magnitude&& frameArray[i].normVelo>0)
+            {
+                best = simi[i].magnitude;
+                bestFrame = i;
+            }
+        }
+
+
+
+        return bestFrame;   
+    }
 
 }
